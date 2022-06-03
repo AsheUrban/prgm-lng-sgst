167404e640ee619f59387681f990a794f901bc54
Ashe Urban <ashe@goldentongue.com>
2022-06-03 12:16:36 -0700

    WIP getting result 3 for undefined conditions (<=60, cereal, painting is still fetching result 3).

--- js/scripts.js
+++ js/scripts.js
@@ -14,54 +14,19 @@ $(document).ready(function() {
     const result3 = "C";
 
     if(age <=20) {
-      if (hobby === "Skiing") {
+      if (operator === "toast");
+      if (hobby === "Skiing") 
         $("#output").text(result1);
-        console.log(result1);
-      }
       
     } else if (age<=40) {
       if (operator === "cereal");
-        $("#output").text(result2);   
- 
+        if (hobby === "Trail Running");
+          $("#output").text(result2);   
      
     } else if (age<=60) {
-      $("#output").text(result3);
+     if (operator === "toast");
+        if (hobby === "painting");
+          $("#output").text(result3);
     }
-    
-    });
+  });
 });
-
-
-// $(document).ready(function() {
-//   $("form#chooseLang").submit(function(event) {
-//     event.preventDefault();
-
-//     const name = $("input#name").val();
-//     const age = $("input#age").val();
-//     const operator = $("input:radio[name=operator]:checked").val();
-//     const hobby = $("input:select[name=option]:selected").val();
-
-//     $("name").text(name);
-//     $("age").text(age);
-//     $("operator").text(operator);
-//     $("hobby").text(hobby);
-
-//     let result1;
-//     if (age <= "20") {
-//       result1=
-
-      
-//     }
-    
-//     $("#output").text(result1);
-
-//     // let result2;
-//     // if ()
-
-//     // $("#output").text(result2);
-
-//     // let result3;
-
-
-//     });
-// });

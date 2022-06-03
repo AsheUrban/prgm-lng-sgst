519e14602723342b9b43956a60c08c120287c66c
Ashe Urban <ashe@goldentongue.com>
2022-06-03 11:01:02 -0700

    WIP function for taking results and choosing a language.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,55 @@
+
+
 //this is ui logic
 
 $(document).ready(function() {
   $("form#chooseLang").submit(function(event) {
     event.preventDefault();
-
-    const name = $("input#name").val();
     const age = $("input#age").val();
-    const operator = $("input:radio[name=operator]:checked").val();
-    const hobby = $("input:select[name=option]:selected").val();
-
-    $("name").text(name);
-    $("age").text(age);
-    $("operator").text(operator);
-    $("hobby").text(hobby);
-
-    let result;
-    if ()
-    
-    $("#output").text(result);
-
+    const result1 = $("Ruby").val();
+    const result2 = $("JavaScript").val();
+    const result3 = $("C#").val();
+    if(age <=20) {
+      $("#output").text(result1);  
+    } else if (age<=40) {
+      $("#output").text(result2);
+    } else {
+      $("#output").text(result3);
+    }
     });
 });
+
+
+// $(document).ready(function() {
+//   $("form#chooseLang").submit(function(event) {
+//     event.preventDefault();
+
+//     const name = $("input#name").val();
+//     const age = $("input#age").val();
+//     const operator = $("input:radio[name=operator]:checked").val();
+//     const hobby = $("input:select[name=option]:selected").val();
+
+//     $("name").text(name);
+//     $("age").text(age);
+//     $("operator").text(operator);
+//     $("hobby").text(hobby);
+
+//     let result1;
+//     if (age <= "20") {
+//       result1=
+
+      
+//     }
+    
+//     $("#output").text(result1);
+
+//     // let result2;
+//     // if ()
+
+//     // $("#output").text(result2);
+
+//     // let result3;
+
+
+//     });
+// });

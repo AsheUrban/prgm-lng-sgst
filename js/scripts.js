c015194b8da525e61751ad783961e7097098df5b
Ashe Urban <ashe@goldentongue.com>
2022-06-03 09:31:09 -0700

    ADD function for inputting and outputting name text. Use console log to confirm value is retrieved and displayed properly.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//this is ui logic
+
+$(document).ready(function() {
+  $("form#chooseLang").submit(function(event) {
+    event.preventDefault();
+    const name = $("input#name").val();
+    const age = $("input#age").val();
+    const operator = $("input:radio[name=operator]:checked").val();
+    console.log(name); // for debugging
+
+    $("name").text(name);
+    // $("age").text(age);
+
+    $("#output").text(name);
+  });
+});
